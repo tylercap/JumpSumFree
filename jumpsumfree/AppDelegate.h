855a394763af0cdaded5704abb0e5d2ffdbdb9e5
cpi.tcap <tcap@openfox.com>
2015-02-20 15:32:22 -0600

    Initial Commit

---
+++ jumpsumfree/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  jumpsumfree
+//
+//  Created by Tyler Cap on 2/20/15.
+//  Copyright (c) 2015 Tyler Cap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ jumpsumfree/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  jumpsumfree
+//
+//  Created by Tyler Cap on 2/20/15.
+//  Copyright (c) 2015 Tyler Cap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
